43cfcf6456c69ab39aad4e3c29cc2b2f422626cd
Muhammad Shafqat <mshafqat.developer@gmail.com>
2025-09-05 21:36:40 +0500

    cookie issue

--- utils/token.js
+++ utils/token.js
@@ -4,19 +4,19 @@ import crypto from 'crypto'
 
 
 export function signAccessToken(payload){
-    return jwt.sign(payload,skhdjkaghruu23i7489haskjy,{expiresIn:1000*60*60*24*7})
+    return jwt.sign(payload,"skhdjkaghruu23i7489haskjy",{expiresIn:1000*60*60*24*7})
 }
 
 export function signRefreshToken(payload){
-    return jwt.sign(payload,skhdjkaghruu23i7489haskjy786324876328746,{expiresIn:1000*60*60*24*7})
+    return jwt.sign(payload,"skhdjkaghruu23i7489haskjy786324876328746",{expiresIn:1000*60*60*24*7})
 }
 
 export function verifyAccessToken(token){
-    return jwt.verify(token,skhdjkaghruu23i7489haskjy,{expiresIn:1000*60*60*24*7})
+    return jwt.verify(token,"skhdjkaghruu23i7489haskjy",{expiresIn:1000*60*60*24*7})
 }
 
 export function verifyRefreshToken(token){
-    return jwt.verify(token,skhdjkaghruu23i7489haskjy786324876328746,{expiresIn:1000*60*60*24*7})
+    return jwt.verify(token,"skhdjkaghruu23i7489haskjy786324876328746",{expiresIn:1000*60*60*24*7})
 }
 
 
